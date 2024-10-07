0559a32ab23c1d46f3d4b9eb50ad9d2ac00eb18c
Matheus de Assis Zordan <83818797+matheus-zordan@users.noreply.github.com>
2024-10-07 09:43:06 -0300

    Update testes7.js

--- estudos/testes7.js
+++ estudos/testes7.js
@@ -1,9 +1,15 @@
-const carro = {
-    marca: 'Marca',
-    preco: 0,
+//Constructor Funcions
+class Carro {
+    constructor(marcaAtribuida, precoAtrbuido) {
+        this.marca = marcaAtribuida
+        this.preco = precoAtrbuido
+    }
 }
+// A palavra new é responsavel por criar um novo objeto baseado na função que passamos a frente dela.
+const honda = new Carro('Honda', 3000)
+const fiat = new Carro('Fiat', 4000)
 
-const honda = carro;
+console.log(honda)
+console.log(fiat)
 
-honda.marca = 'Honda'
-honda.preco = 50000
+
